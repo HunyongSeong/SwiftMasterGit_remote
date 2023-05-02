ef8d59b58cccf443a3747c2066aa1b1a67089345
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-02 16:17:58 +0900

    Initial Commit

---
+++ SwiftMasterGit/SwiftMasterGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMasterGitApp.swift
+//  SwiftMasterGit
+//
+//  Created by David Goggins on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMasterGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
