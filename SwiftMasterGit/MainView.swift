35b2c4069ad0e7dcc10f62ef660b380a4fb63fbd
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-02 23:59:46 +0900

    to blue

--- SwiftMasterGit/MainView.swift
+++ SwiftMasterGit/MainView.swift
@@ -11,7 +11,7 @@ struct MainView: View {
     var body: some View {
         ZStack{
             
-            Color.red.ignoresSafeArea(.all)
+            Color.blue.ignoresSafeArea(.all)
             
             VStack {
                 Image(systemName: "globe")
