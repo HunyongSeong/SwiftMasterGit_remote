df1d5ad8c8190d889c1a2b78f7aaf78a197dfe93
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-02 20:28:18 +0900

    Change text2

--- SwiftMasterGit/ContentView.swift
+++ SwiftMasterGit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("텍스트 변경")
+            Text("텍스트 변경2")
         }
         .padding()
     }
